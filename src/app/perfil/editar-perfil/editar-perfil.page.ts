5d007d05464033de048839ddb520e452fcda00e1
lurapozo <lurapozo@espol.edu.ec>
2022-09-17 18:00:24 -0500

    Permitir cambiar cedula en editar perfil

--- src/app/perfil/editar-perfil/editar-perfil.page.ts
+++ src/app/perfil/editar-perfil/editar-perfil.page.ts
@@ -83,18 +83,26 @@ export class EditarPerfilPage implements OnInit {
     formData.append("id", this.perfil.id);
     formData.append("nombre",this.perfil.nombre);    
     formData.append("apellido",this.perfil.apellido);
+    formData.append("cedula",this.perfil.cedula);
     formData.append("telefono",this.perfil.telefono);
     formData.append("direccion",this.perfil.direccion);
     formData.append("fechaNac",this.perfil.fechaNac);
     formData.append("url",this.formData.get("file"))
     form = form.value;
-    if(form.nombre == ''|| form.apellido == ''){
+    if(form.nombre == ''|| form.apellido == '' || form.cedula == '' ){
       this.mensajeIncorrecto("Campos Incompletos","Por favor complete los campos Nombre y Apellido");
-    }else{
+      var int_length = form.cedula.length;
+    } else if (!this.validarCedula(form.cedula)) {
+        this.mensajeIncorrecto("Revisar cédula", "Su cédula no es válida");
+    } else if ((int_length < 10 && int_length < 13) || int_length > 13) {
+        this.mensajeIncorrecto("Revisar RUC", "Recuerde que si ingresa cédula deben ser  RUC 13 dígitos");
+    }
+    else{
       let regExp = /^[0-9]{10}$/;
       if (form.telefono != "" && !regExp.test(form.telefono)) {
         this.mensajeIncorrecto("Campo incorrecto","Número de teléfono debe contener 10 números");
       }else{
+        console.log(...formData)
         this.editarPerfil(formData);
       }
 
@@ -173,4 +181,68 @@ export class EditarPerfilPage implements OnInit {
     this.navCtrlr.back(animations)
   }
 
+  validarCedula(cedula: string) {
+    // Créditos: Victor Diaz De La Gasca.
+    // Autor: Adrián Egüez
+    console.log(cedula)
+    if (cedula.length === 10) {
+      const digitoRegion = cedula.substring(0, 2);
+      console.log(digitoRegion)
+      if (Number(digitoRegion) >= 1 && Number(digitoRegion) <= 24) {
+        const ultimoDigito = Number(cedula.substring(9, 10));
+        const pares = Number(cedula.substring(1, 2)) + Number(cedula.substring(3, 4)) + Number(cedula.substring(5, 6)) + Number(cedula.substring(7, 8));
+        let numeroUno: any = cedula.substring(0, 1);
+        numeroUno = (numeroUno * 2);
+        if (numeroUno > 9) {
+          numeroUno = (numeroUno - 9);
+        }
+  
+        let numeroTres: any = cedula.substring(2, 3);
+        numeroTres = (numeroTres * 2);
+        if (numeroTres > 9) {
+          numeroTres = (numeroTres - 9);
+        }
+  
+        let numeroCinco: any = cedula.substring(4, 5);
+        numeroCinco = (numeroCinco * 2);
+        if (numeroCinco > 9) {
+          numeroCinco = (numeroCinco - 9);
+        }
+  
+        let numeroSiete: any = cedula.substring(6, 7);
+        numeroSiete = (numeroSiete * 2);
+        if (numeroSiete > 9) {
+          numeroSiete = (numeroSiete - 9);
+        }
+  
+        let numeroNueve: any = cedula.substring(8, 9);
+        numeroNueve = (numeroNueve * 2);
+        if (numeroNueve > 9) {
+          numeroNueve = (numeroNueve - 9);
+        }
+        const impares = numeroUno + numeroTres + numeroCinco + numeroSiete + numeroNueve;
+        const sumaTotal = (pares + impares);
+        const primerDigitoSuma = String(sumaTotal).substring(0, 1);
+        const decena = (Number(primerDigitoSuma) + 1) * 10;
+        console.log(decena)
+        let digitoValidador = decena - sumaTotal;
+        console.log(digitoValidador)
+        if (digitoValidador === 10) {
+          digitoValidador = 0;
+        }
+        console.log(digitoValidador)
+        if (digitoValidador === ultimoDigito) {
+          return true;
+        } else {
+          return false;
+        }
+  
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  
+  }
 }
